4488709013f3ee730bbf9eab6d8b7d1023b2fc33
azametelsky <azametelsky@it.ru>
2019-05-08 16:55:15 +0300

    Add env EXPRESS_JSON_LIMIT

--- src/robot.js
+++ src/robot.js
@@ -426,6 +426,7 @@ class Robot {
     const port = process.env.EXPRESS_PORT || process.env.PORT || 8080
     const address = process.env.EXPRESS_BIND_ADDRESS || process.env.BIND_ADDRESS || '0.0.0.0'
     const limit = process.env.EXPRESS_LIMIT || '100kb'
+	const jsonLimit = process.env.EXPRESS_JSON_LIMIT || '100kb'
     const paramLimit = parseInt(process.env.EXPRESS_PARAMETER_LIMIT) || 1000
 
     const express = require('express')
@@ -443,7 +444,7 @@ class Robot {
     }
     app.use(express.query())
 
-    app.use(express.json())
+    app.use(express.json({ limit: jsonLimit }));
     app.use(express.urlencoded({ limit, parameterLimit: paramLimit, extended: true }))
     // replacement for deprecated express.multipart/connect.multipart
     // limit to 100mb, as per the old behavior
